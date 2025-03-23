a3e6bce9f9e564c43f7e62df5fdf7716f27e0a2d
Jakob Jónasson <jakobjo@tempo.io>
2025-03-23 21:37:14 +0000

    Add Harpoon

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -41,6 +41,20 @@ vim.keymap.set("n", "<leader>ts", function() neotest.run.stop() end, { desc = "S
 vim.keymap.set("n", "<leader>ta", function() neotest.run.attach() end, { desc = "Attach to nearest test" })
 vim.keymap.set("n", "<leader>ts", function() neotest.summary.toggle() end, { desc = "Toggle summary" })
 
+-- Harpoon
+local hmark = require("harpoon.mark")
+local hui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>ha", function() hmark.add_file() end, { desc = "Add file" })
+vim.keymap.set("n", "<leader>hm", function() hui.toggle_quick_menu() end, { desc = "Toggle menu" })
+vim.keymap.set("n", "<leader>hn", function() hui.nav_next() end, { desc = "Nav next" })
+vim.keymap.set("n", "<leader>tp", function() hui.nav_prev() end, { desc = "Nav prev" })
+
+vim.keymap.set("n", "<leader>hh", function() hui.nav_file(1) end, { desc = "Nav to 1" })
+vim.keymap.set("n", "<leader>hj", function() hui.nav_file(2) end, { desc = "Nav to 2" })
+vim.keymap.set("n", "<leader>hk", function() hui.nav_file(3) end, { desc = "Nav to 3" })
+vim.keymap.set("n", "<leader>hl", function() hui.nav_file(4) end, { desc = "Nav to 4" })
+
 -- Comment out and in
 vim.api.nvim_set_keymap('v', '<leader>/', ':<C-u>lua ToggleComment()<CR>', { noremap = true, silent = true })
 
