bd8531289e8c216a1a68e215b5af1751b0312475
Jakob Jónasson <jakobjo@tempo.io>
2025-04-08 22:28:51 +0000

    Keymap updates

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,16 +1,35 @@
 -- Common
+
 -- Map <Leader><Tab> to switch to the last opened file
 vim.api.nvim_set_keymap('n', '<Leader><Tab>', ':b#<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk'})
+vim.keymap.set('n', '<leader>nh', ':nohl<CR>', { desc = 'Clear searchn highlights'})
+
+-- Increment / Decrement Numbers
+vim.keymap.set('n', '<leader>+', '<C-a>', { desc = 'Increment number'})
+vim.keymap.set('n', '<leader>-', '<C-x>', { desc = 'Decrement number'})
+
+-- Window management
+vim.keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically'})
+vim.keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horizontally'})
+vim.keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Makes split equal size'})
+vim.keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split'})
+
+vim.keymap.set('n', '<leader>tbo', '<cmd>tabnew<CR>', { desc = 'Open new tab'})
+vim.keymap.set('n', '<leader>tbx', '<cmd>tabclose<CR>', { desc = 'Close current tab'})
+vim.keymap.set('n', '<leader>tbn', '<cmd>tabn<CR>', { desc = 'Go to next tab'})
+vim.keymap.set('n', '<leader>tbp', '<cmd>tabp<CR>', { desc = 'Go to previous tab'})
+vim.keymap.set('n', '<leader>tbf', '<cmd>tabnew %<CR>', { desc = 'Open current buffer in new tab'})
 
 -- Disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- Tree maps
-vim.keymap.set('n', '<leader>nt', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>nf', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>nff', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>nc', ':NvimTreeCollapse<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>nt', ':NvimTreeToggle<CR>', { noremap = true, silent = true, desc = 'Toggle file explorer' })
+vim.keymap.set('n', '<leader>nf', ':NvimTreeFocus<CR>', { noremap = true, silent = true, desc = 'Focus on tree'})
+vim.keymap.set('n', '<leader>nn', ':NvimTreeFindFile<CR>', { noremap = true, silent = true, desc = 'Filter on file in tree' })
+vim.keymap.set('n', '<leader>nc', ':NvimTreeCollapse<CR>', { noremap = true, silent = true, desc = 'Close file tree' })
 
 -- Coc mapping
 vim.api.nvim_set_keymap("i", "<Enter>", [[pumvisible() ? coc#_select_confirm() : "\<CR>"]], { expr = true, noremap = true, silent = true })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,12 +1,28 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set expandtab") -- expand tabs to spaces
+vim.cmd("set tabstop=4") -- 4 spaces for tabs
+vim.cmd("set softtabstop=4") -- 4 spaces while in insert mode
+vim.cmd("set shiftwidth=4") -- 4 spaces for indent
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.wo.number = true
 vim.opt.termguicolors = true
-
+vim.opt.background = "dark"
+vim.opt.signcolumn = "yes"
 vim.opt.backupcopy = "yes"
+  
+vim.wo.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true 
+
+vim.opt.backspace = "indent,eol,start"
+vim.opt.clipboard:append("unnamedplus") -- system clipboard
+
+-- split windows
+vim.opt.splitright = true
+vim.opt.splitbelow = true
