02169098c147cd050a4897fbba0649396d5bce6b
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-04-13 23:05:56 +0000

    More improvements

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -38,8 +38,8 @@ vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Telescope find rec
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fc', builtin.grep_string, { desc = 'Telescope grep string' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>fgit', builtin.git_status, { desc = 'Telescope git status' })
+vim.keymap.set('n', '<leader>fhe', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fgi', builtin.git_status, { desc = 'Telescope git status' })
 vim.api.nvim_set_keymap('n', '<leader>fi', ':Telescope import<CR>', { noremap = true, silent = true })
 
 -- Noetest
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,9 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    "nvim-telescope/telescope.nvim", tag = "0.1.8",
     dependencies = { 
-        'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-        'nvim-tree/nvim-web-devicons'
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons"
     },
     config = function()
         local telescope = require("telescope")
@@ -11,18 +11,18 @@ return {
 
         telescope.setup{
             defaults = {
-                file_ignore_patterns = {'node_modules/', 'dist/', 'bin/'},
+                file_ignore_patterns = {"node_modules/", "dist/", "bin/"},
                 path_display = { "smart" },
                 mappings = {
                     i = {
-                        ['<C-k>'] = actions.move_selection_previous,
-                        ['<C-j>'] = actions.move_selection_next,
-                        ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
                     }
                 }
             }
         }
 
-        telescope.load_extension('fzf')
+        telescope.load_extension("fzf")
     end
 }
