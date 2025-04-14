5f8a307e4004fd774a6e19bea05cf3e2a0b3f1db
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-04-14 18:13:28 +0000

    linting

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,45 +1,47 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim"
-    },
-    config = function()
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
-        local mason_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_installer = require("mason-tool-installer")
 
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "",
-                    package_pending = "󰝲",
-                    package_uninstalled = "q"
-                }
-            }
-        })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "",
+					package_pending = "󰝲",
+					package_uninstalled = "q",
+				},
+			},
+		})
 
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "ts_ls",
-                "html",
-                "cssls",
-                "tailwindcss",
-                "svelte",
-                "lua_ls",
-                "pyright",
-                "jdtls",
-                "kotlin_language_server"
-            }
-        })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"ts_ls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"pyright",
+				"jdtls",
+				"kotlin_language_server",
+			},
+		})
 
-        mason_installer.setup({
-            ensure_installed = {
-                "prettier",
-                "stylua",
-                "isort",
-                "black"
-            }
-        })
-    end
+		mason_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
+				"pylint",
+				"eslint_d",
+			},
+		})
+	end,
 }
