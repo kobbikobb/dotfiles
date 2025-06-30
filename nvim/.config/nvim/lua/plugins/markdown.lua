4c817493d00fcbe71b02675f44e6e73a2d9990dd
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-06-30 15:57:35 +0000

    fix markdown preview

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,9 +1,7 @@
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = function()
-		vim.fn["mkdp#util#install"]()
-	end,
+	build = "cd app && npm install",
 	init = function()
 		vim.g.mkdp_filetypes = { "markdown" }
 
